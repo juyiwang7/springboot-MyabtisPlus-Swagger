a7cc69eba1fdd751d0f30722e1da96583f2828b3
weisan <1243574274@qq.com>
2020-05-16 13:39:47 +0800

    测试文本检测

--- src/test/java/com/example/bootMp/BootMpApplicationTests.java
+++ src/test/java/com/example/bootMp/BootMpApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.bootMp;
 
+import com.zhixiangli.code.similarity.CodeSimilarity;
+import com.zhixiangli.code.similarity.strategy.CosineSimilarity;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BootMpApplicationTests {
 
+	//测试相似度检测代码
 	@Test
 	public void contextLoads() {
+		String a = "public static void main(String[] args) {System.out.println(1);}";
+		String b = "public static void main(String[] args) {System.out.println(2);}";
+		CodeSimilarity codeSimilarity = new CodeSimilarity();
+		System.out.println(codeSimilarity.get(a, b));
+
+		CodeSimilarity cosineSimilarity = new CodeSimilarity(new CosineSimilarity());
+		System.out.println(cosineSimilarity.get(a, b));
 	}
 
 }
